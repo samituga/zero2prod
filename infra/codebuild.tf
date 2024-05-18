333dbd235671fdaa8484164062597077d9f8c6df
Samuel <samuel.c.goncalves07@gmail.com>
2024-05-18 13:12:38 +0100

    Change codebuild image

--- infra/codebuild.tf
+++ infra/codebuild.tf
@@ -6,7 +6,7 @@ resource "aws_codebuild_project" "rust_server_build" {
   }
   environment {
     compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+    image           = "aws/codebuild/standard:7.0"
     type            = "LINUX_CONTAINER"
     privileged_mode = true
 
