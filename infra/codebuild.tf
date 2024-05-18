a0e62b2d22c7b7ce7e2bb6e05938f2216ed3294a
Samuel <samuel.c.goncalves07@gmail.com>
2024-05-18 12:14:09 +0100

    Add codebuild_s3_access

--- infra/codebuild.tf
+++ infra/codebuild.tf
@@ -49,3 +49,8 @@ resource "aws_iam_role_policy_attachment" "codebuild_cloudwatch_access" {
   role       = aws_iam_role.codebuild.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
+
+resource "aws_iam_role_policy_attachment" "codebuild_s3_access" {
+  role       = aws_iam_role.codebuild.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
