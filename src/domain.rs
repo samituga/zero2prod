a9ff6fea97d2d5c15188af6004f692e7571bc3a0
Samuel <samuel.c.goncalves07@gmail.com>
2024-06-02 16:46:14 +0100

    Parse SubscriberName

---
+++ src/domain.rs
@@ -0,0 +1,119 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+pub struct NewSubscriber {
+    pub email: String,
+    pub name: SubscriberName,
+}
+
+#[derive(Debug)]
+pub struct SubscriberName(String);
+
+impl SubscriberName {
+    pub fn inner(self) -> String {
+        self.0
+    }
+
+    pub fn inner_mut(&mut self) -> &mut str {
+        &mut self.0
+    }
+
+    pub fn parse(s: String) -> Result<SubscriberName, String> {
+        let is_empty_or_whitespace = s.trim().is_empty();
+        let is_too_long = s.graphemes(true).count() > 256;
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            Err(format!("{} is not a valid subscriber name.", s))
+        } else {
+            Ok(Self(s))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use claims::{assert_err, assert_ok};
+
+    use crate::domain::SubscriberName;
+
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        // Arrange
+        let name = "ё".repeat(256);
+
+        // Act
+        let result = SubscriberName::parse(name);
+
+        // Assert
+        assert_ok!(result);
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        // Arrange
+        let name = "ё".repeat(257);
+
+        // Act
+        let result = SubscriberName::parse(name);
+
+        // Assert
+        assert_err!(result);
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        // Arrange
+        let name = " ".to_string();
+
+        // Act
+        let result = SubscriberName::parse(name);
+
+        // Assert
+        assert_err!(result);
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        // Arrange
+        let name = "".to_string();
+
+        // Act
+        let result = SubscriberName::parse(name);
+
+        // Assert
+        assert_err!(result);
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        // Arrange
+        let invalid_characters = &['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+
+        for name in invalid_characters {
+            let name = name.to_string();
+
+            // Act
+            let result = SubscriberName::parse(name);
+
+            // Assert
+            assert_err!(result);
+        }
+    }
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        // Arrange
+        let name = "Ursula Le Guin".to_string();
+
+        // Act
+        let result = SubscriberName::parse(name);
+
+        // Assert
+        assert_ok!(result);
+    }
+}
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -57,6 +57,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let connection_pool = PgPool::connect_with(config.with_db_name())
         .await
         .expect("Failed to connect to Postgres.");
+
     sqlx::migrate!("./migrations")
         .run(&connection_pool)
         .await
@@ -136,5 +137,35 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             "The API did not fail with 400 Bad Request when the payload was {}.",
             error_message
         );
+    }
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_when_fields_are_present_but_invalid() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a  400 Bad Request when the payload was {}.",
+            description
+        )
     }
 }
