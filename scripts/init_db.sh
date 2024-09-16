8cad7fe47cca0533ecbbcf4aadc06b32795a15f3
Samuel Gonçalves <43956748+samituga@users.noreply.github.com>
2024-09-16 16:09:00 +0100

    Don't require psql installed

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,11 +2,6 @@
 set -x
 set -eo pipefail
 
-if ! [ -x "$(command -v psql)" ]; then
-  echo >&2 "Error: psql is not installed."
-  exit 1
-fi
-
 if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed."
   echo >&2 "Use:"
@@ -22,23 +17,44 @@ DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
+# Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]; then
+  # if a postgres container is running, print instructions to kill it and exit
+  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+    echo >&2 "there is a postgres container already running, kill it with"
+    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
+    exit 1
+  fi
+  CONTAINER_NAME="postgres_$(date '+%s')"
+  # Launch postgres using Docker
   docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres:16.2 \
-    postgres -N 1000
-fi
+      -e POSTGRES_USER="${DB_USER}" \
+      -e POSTGRES_PASSWORD="${DB_PASSWORD}" \
+      -e POSTGRES_DB="${DB_NAME}" \
+      --health-cmd="pg_isready -U ${DB_USER} || exit 1" \
+      --health-interval=1s \
+      --health-timeout=5s \
+      --health-retries=5 \
+      -p "${DB_PORT}":5432 \
+      -d \
+      --name "${CONTAINER_NAME}" \
+      postgres -N 1000
+      # ^ Increased maximum number of connections for testing purposes
+
 
 export PGPASSWORD="${DB_PASSWORD}"
 
-until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-  echo >&2 "Postgres is still unavailable - sleeping"
-  sleep 2
-done
-echo >&2 "Postgres is up and running on port ${DB_PORT}!"
+  until [ \
+    "$(docker inspect -f "{{.State.Health.Status}}" "${CONTAINER_NAME}")" == \
+    "healthy" \
+  ]; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+  done
+fi
+
+echo >&2 "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
